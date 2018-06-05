48b7d18998e745021800dd4371ac123c7e3c3c5d
yukq16 <shikine.yui@gmail.com>
2018-06-05 14:32:39 +0900

    modified a fixed sourcetree

--- install.sh
+++ install.sh
@@ -1,38 +1,17 @@
 #!/bin/bash
 
-set -u
+DOT_DIRECTORY="${HOME}/dotfiles"
 
-# 実行場所のディレクトリを取得
-THIS_DIR=$(cd $(dirname $0); pwd)
+# ディレクトリがなかったらダウンロードする設定
+# 追加予定
 
-cd $THIS_DIR
-git submodule init
-git submodule update
+cd ${DOT_DIRECTORY}
 
-echo "start setup..."
-for f in .??*; do
-    [ "$f" = ".git" ] && continue
-    [ "$f" = ".gitconfig.local.template" ] && continue
-    [ "$f" = ".require_oh-my-zsh" ] && continue
-    [ "$f" = ".gitmodules" ] && continue
-
-    ln -snfv ~/dotfiles/"$f" ~/
+for f in .??*;do
+# 無視したいファイルやディレクトリ
+    [${f}=".git"] && continue
+    [${f}=".gitignore"] && continue
+    ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
 done
 
-[ -e ~/.gitconfig.local ] || cp ~/dotfiles/.gitconfig.local.template ~/.gitconfig.local
-
-# emacs set up
-if which cask >/dev/null 2>&1; then
-  echo "setup .emacs.d..."
-  cd ${THIS_DIR}/.emacs.d
-  cask upgrade
-  cask install
-fi
-
-cat << END
-
-**************************************************
-DOTFILES SETUP FINISHED! bye.
-**************************************************
-
-END
+echo "Setup dotfiles!"
